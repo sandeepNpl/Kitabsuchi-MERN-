6dbb72337ee9516da04e69745cadb89cb56acde0
Sandip Nepali <sandeep47nepali@gmail.com>
2023-02-11 23:30:57 +0545

    added admin page

---
+++ client/src/containers/admin/adminDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Logout from "../../components/logout";
+import Modal from "./../../components/modal"
+const adminDashboard = () => {
+
+  return (
+    <div>
+    <Logout/>
+    <Modal/>
+
+    </div>
+  )
+}
+
+export default adminDashboard
