a216f8e1a32ceaa4089b8a8ede51dab6dfb51877
sandip <sandeep47nepali@gmail.com>
2023-03-14 06:52:03 +0545

    changes some UI

---
+++ client/src/components/ourService/Service.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./service.css"
+
+const Service = () => {
+  return (
+    <>
+    <h2 className='service-header'>Our Services</h2>
+    <div className='service-container'>
+      <div className='addBookForSale'>
+       <h2>Add Book For Sale</h2>
+
+      </div>
+
+      <div className='requestForBook'>
+      <h2>Request For Sale</h2>
+
+      </div>
+
+
+      <div className='chatWithSeller'>
+      <h2>Chat With Seller </h2>
+
+      </div>
+
+      <div className="buyAtCheapPrice">
+      <h2>Buy Book at cheap price</h2>
+      </div>
+      
+    </div>
+
+    </>
+    
+  )
+}
+
+export default Service
--- client/src/containers/user/buyerDashboard.js
+++ client/src/containers/user/buyerDashboard.js
@@ -7,6 +7,7 @@ import Logo from "./../../components/header/logo/Logo";
 import Navbar from "../../components/header/navbar/Navbar";
 import LeftSide from "../../components/mainSection/leftSide/LeftSide";
 import RightSide from "../../components/mainSection/rightSide/RightSide";
+import Service from "../../components/ourService/Service";
 import { textAlign } from "@mui/system";
 
 const BuyerDashboard = () => {
@@ -33,16 +34,20 @@ const BuyerDashboard = () => {
           style={{
             display: "flex",
             justifyContent: "space-between",
-            marginTop: "4%",
+            marginTop: "3%",
           }}
         >
-          <div className="">
+          <div className="left-section">
             <LeftSide />
           </div>
-          <div className="">
+          <div className="right-section">
             <RightSide />
           </div>
         </div>
+
+        <div className="service-section">
+            <Service />
+          </div>
 
         <div>{/* <Fetch /> */}</div>
       </div>
