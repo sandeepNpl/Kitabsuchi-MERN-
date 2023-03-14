57473d710b73607ee01d6bc5e50abf2150ad416d
sandip <sandeep47nepali@gmail.com>
2023-03-14 06:49:00 +0545

    change style for leftside of main section

--- client/src/components/mainSection/leftSide/LeftSide.js
+++ client/src/components/mainSection/leftSide/LeftSide.js
@@ -4,21 +4,22 @@ const LeftSide = () => {
   return (
     <>
       <div className="left-side-container">
-      <h2 className="leftSide-hearder">
-        Your go-to destination for second-hand b
-        <span className="circle-style">oo</span>ks
-      </h2>
+        <h2 className="leftSide-hearder">
+          Your go-to destination for second-hand b
+          <span className="circle-style">oo</span>ks
+        </h2>
 
-      <p className="home-description">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s.
-      </p>
+        <p className="home-description">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s.There are many variations of passages of Lorem Ipsum
+          available, but the majority have suffered alteration in some form, by
+        </p>
 
-      <div className="left-side-button">
-      <button className="explore-button">Explore</button>
-      <button className="getStarted-button">Get Started</button>
-      </div>
+        <div className="left-side-button">
+          <button className="explore-button">Explore</button>
+          <button className="getStarted-button">Get Started</button>
+        </div>
       </div>
     </>
   );
