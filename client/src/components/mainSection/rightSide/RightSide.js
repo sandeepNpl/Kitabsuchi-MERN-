fb88a4eec538c047e50aac044233a2989aec60b2
sandip <sandeep47nepali@gmail.com>
2023-03-14 06:50:49 +0545

    fix UI for the right side of the main section

--- client/src/components/mainSection/rightSide/RightSide.js
+++ client/src/components/mainSection/rightSide/RightSide.js
@@ -3,7 +3,7 @@ import Illustrate from "../../../assets/images/Imagination-bro.png"
 const RightSide = () => {
   return (
     <div className='rightSide-container'>
-     <img src= {Illustrate} width="400px" />
+     <img src= {Illustrate} width="350px" />
     </div>
   )
 }
